2c4e553c9c2556ce2a1424a4a7dfa457a7d86560
Livio Alvarenga <livioalvarenga@gmail.com>
2025-05-02 17:43:49 -0300

    feat: add Button component and update login page with multiple button variants

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 import { metadata as metadataHome } from '@/app/layout'
 import { Text } from '@/components/Text'
+import { Button } from '@/components/ui/button'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -12,9 +13,18 @@ export default async function Login() {
   return (
     <div
       id="main"
-      className="from-primary to-tertiary flex flex-1 flex-col items-center justify-center bg-gradient-to-b px-4 py-5"
+      className="from-primary to-tertiary flex flex-1 flex-col items-center justify-center gap-5 bg-gradient-to-b px-4 py-5"
     >
-      <Text variant="headline-24-45-700">Tela de login</Text>
+      <Text variant="headline-24-45-700" className="text-primary-foreground">
+        Tela de login
+      </Text>
+
+      <Button variant="default">default button</Button>
+      <Button variant="destructive">destructive button</Button>
+      <Button variant="outline">outline button</Button>
+      <Button variant="secondary">secondary button</Button>
+      <Button variant="ghost">ghost button</Button>
+      <Button variant="link">link button</Button>
     </div>
   )
 }
