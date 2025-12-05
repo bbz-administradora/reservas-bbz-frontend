ddbfe50967d9397f5c1660ac3765e7174c068c00
Livio Alvarenga <livioalvarenga@gmail.com>
2025-12-05 20:14:16 -0300

    fix(revalidate-tags): update revalidateTags function to use Promise.all for concurrent execution

--- src/actions/revalidate-tags.ts
+++ src/actions/revalidate-tags.ts
@@ -4,5 +4,5 @@ import { revalidateTag } from 'next/cache'
 
 // Iterates over the provided tags and revalidates each tag concurrently.
 export async function revalidateTags(tags: string[]): Promise<void> {
-  await Promise.all(tags.map((tag) => revalidateTag(tag)))
+  tags.forEach((tag) => revalidateTag(tag, 'max'))
 }
